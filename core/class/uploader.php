9f2bc202435d282f92121dc14cb13f467590924b
Eccenux <eccenux+git@gmail.com>
2016-04-27 16:37:21 +0200

    fix: even when normalizing names overwritten file can contain `(1)`.

--- core/class/uploader.php
+++ core/class/uploader.php
@@ -369,7 +369,7 @@ class uploader {
             (
                 isset($this->config['_normalizeFilenames']) &&
                 $this->config['_normalizeFilenames'] &&
-                preg_match('/[^0-9a-z\.\- _]/si', $file)
+                preg_match('/[^0-9a-z\.\- _\(\)]/si', $file)	// note `(1)` is added to file name when preventing overwrite
             )
         )
             return false;
